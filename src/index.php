fb70c280724205d5be608a7e59a61f26ccb7abdd
JonathanACS <jonathan.beze580@gmail.com>
2024-05-27 15:17:36 +0200

    tableau stage

---
+++ src/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tableau Crud PHP</title>
+</head>
+<body>
+    <h1>Tableau crud stage</h1>
+    <table>
+        <thead>
+            <td>id</td>
+            <td>statut</td>
+            <td>entreprise</td>
+            <td>date</td>
+            <td>date_de_relance</td>
+            <td>type_de_postulation</td>
+            <td>website</td>
+            <td>intitulé_du_poste</td>
+            <td>type_de_contract</td>
+            <td>mail</td>
+            <td>commentaires</td>
+        </thead>
+        <tbody>
+            <td>1</td>
+            <td>pris</td>
+            <td>souris</td>
+            <td>2024-05-01</td>
+            <td>2024-05-31</td>
+            <td>rose</td>
+            <td>recherche</td>
+            <td>développeur web</td>
+            <td>stage</td>
+            <td>123@mail.com</td>
+            <td>voici un commentaire</td>
+        </tbody>
+    </table>
+</body>
+</html>
