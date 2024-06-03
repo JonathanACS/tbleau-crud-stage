46e92ca6dc253cee90467b4fb9972d23b15aaeae
JonathanACS <jonathan.beze580@gmail.com>
2024-06-03 14:14:04 +0200

    correction

--- src/index.php
+++ src/index.php
@@ -15,7 +15,6 @@
 
 <body>
     <?php include_once("./include/nav.php");?>
-    <!-- <h1 class="title-center">Accueil tableau crud</h1> -->
     <h1 class="title-center">Tableau CRUD en PHP</h1>
     <p class="title-center">Gérez vos candidatures de stage et d'emploi facilement et efficacement</p>
     <section>
@@ -28,19 +27,19 @@
             <div class="card">
                 <h2>Voir toutes les candidatures</h2>
                 <p>Consultez toutes vos candidatures de stage</p>
-                <a href="stage.php"><button>Voir les candidatures</button></a>
+                <a href="stage.php"><button>Voir les recherche de stage</button></a>
             </div>
         </div>
         <div class="container">
             <div class="card">
                 <h2>Ajouter une recherche d'emploi</h2>
                 <p>Enregistrez vos candidatures et suivez leur statut</p>
-                <a href="add_emploie.php"><button>Ajouter un stage</button></a>
+                <a href="add_emploie.php"><button>Ajouter une recherche d'emploi</button></a>
             </div>
             <div class="card">
                 <h2>Voir toutes les candidatures</h2>
                 <p>Consultez toutes vos candidatures d'emploi</p>
-                <a href="emploie.php"><button>Voir les candidatures</button></a>
+                <a href="emploie.php"><button>Voir les recherche d'emploi</button></a>
             </div>
         </div>
     </section>
